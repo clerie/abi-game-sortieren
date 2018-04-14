3b2cc76c8cc1db608b769e45f70b691c02fca5e2
clerie <clemens@milchinsel.de>
2018-04-14 12:40:57 +0200

    Added dependencies

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const http_port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:' + http_port);
+});
